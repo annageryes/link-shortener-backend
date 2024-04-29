f707fa8feb65670ea2ef018bd8e32675f1c0679e
asakran <asakran@habana.ai>
2024-04-29 23:56:48 +0300

    check if url exists

--- link_shortener.py
+++ link_shortener.py
@@ -13,13 +13,25 @@ class LinkShortener:
 
     @classmethod
     def shorten(self, url: str):
+        id_exists=self.url_exists(url) 
+        if id_exists:
+            return id_exists
         unique_id = str(uuid.uuid4())[:8]
         new_url_entity = {"id": unique_id, "url": url}
         url_entities = JsonManager.read()
         url_entities.append(new_url_entity)
         JsonManager.write(url_entities)
         return unique_id
-
+    
+    @classmethod
+    def url_exists(self,url: str):
+        url_entities = JsonManager.read()
+        for url_entity in url_entities:
+            if url_entity["url"] == url:
+                return id
+        return None
+        
+        
 
 
 
